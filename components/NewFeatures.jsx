f779c395ba4a7f9e115ace0bf808c4dd64b9d58e
yassin <y.attuo@esi-sba.dz>
2023-02-17 12:41:07 +0100

    what's new  section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,16 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from "../styles";
+const NewFeatures = ({imgUrl,title,subtitle}) => (
+  <div className =" flex-1 flex flex-col sm:max-[250px] min-w-[210px]">
+    <div className={`${styles.flexCenter} w-[70px] h-[70px]  rounded-[24px] bg-[323f5d]`}>
+        <img 
+          src={imgUrl}
+          alt="feature"
+          className="object-contain w-1/2 h-1/2"
+        />
+         </div>
+        <h1 className="mt-6 text-6 leading-7 text-white font-bold ">{title}</h1>
+        <p className="flex-1 mt-4 font-normal text-[18px] text-[#b0b0b0] ">{subtitle}</p>
+   
   </div>
 );
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,8 +1,45 @@
 'use client';
+import {motion} from "framer-motion";
+import { TypingText,TitleText } from "../components/CustomTexts.jsx";
+import  NewFeatures from "../components/NewFeatures.jsx";
+
+import styles from "../styles/index.js";
+import {staggerContainer ,fadeIn,planetVariants} from "../utils/motion"
+import {newFeatures} from "../constants/index.js"
 
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div 
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport={{once:false , amount:0.25}}
+    className={`flex gap-8 mx-auto ${styles.innerWidth} lg:flex-row-reverse flex-col-reverse `}>
+        <motion.div 
+        variants={planetVariants('right')}
+        className={`${styles.flexCenter} flex-1`}>
+          <img src="./whats-new.png"
+          alt="getStarted" 
+            className="w-[90%] h-[90%] object-contain"
+          />
+        </motion.div>
+        <motion.div
+        variants={fadeIn('right','tween',0.2,1)}
+        className='flex flex-col flex-[0.75] justify-cneter'
+        >
+          <TypingText title='| Whats New' />
+          <TitleText title ={<>What's new About Metaversus</>} 
+           />
+           <div
+           className  ="flex flex-wrap justify-between  mt-12 gap-6">
+
+              {newFeatures.map((feature) => (
+                <NewFeatures key={feature.title} {...feature}  />
+              ))}
+           </div>
+          
+        </motion.div>
+    </motion.div>
   </section>
 );
 
