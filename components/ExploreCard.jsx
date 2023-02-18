bcd353c5a05b5ee166e646f979e4bc6dbee7b796
yassin <y.attuo@esi-sba.dz>
2023-02-18 13:11:46 +0100

    deploy version2

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,46 +1,52 @@
 'use client';
 
-import {motion} from "framer-motion";
-import styles from "../styles/index.js";
-import {fadeIn} from "../utils/motion"
+import { motion } from 'framer-motion';
+import styles from '../styles/index.js';
+import { fadeIn } from '../utils/motion';
 
-const ExploreCard = ({id,imgUrl,title,index,active,handleClick}) => (
+const ExploreCard = ({
+  id, imgUrl, title, index, active, handleClick,
+}) => (
   <motion.div
-      variants={fadeIn("right","spring",index * 0.1,0.75)}
-     initial="hidden"
+    variants={fadeIn('right', 'spring', index * 0.1, 0.75)}
+    initial="hidden"
     whileInView="show"
-    viewport={{once:false ,amount:0.5}}
-    className={`${active ===id ? "lg:flex-[3.5] flex-[10] ":"lg:[0.5] flex-[2]"} 
+    viewport={{ once: false, amount: 0.5 }}
+    className={`${active === id ? 'lg:flex-[3.5] flex-[10] ' : 'lg:[0.5] flex-[2]'} 
     flex items-center justify-center min-w-[170px] h-[350px] transition-[flex] duration-[0.75s]
     ease-out-flex cursor-pointer
      relative`}
-     onClick={()=>handleClick(id)}
+    onClick={() => handleClick(id)}
   >
-  <img  
-    src={imgUrl}
-    alt="image-world"
-    className="absolute w-full h-full object-cover rounded-[24px]"
-  />
-  {active !==id ? (
-    <h2 className="text-white absolute z-0 text-[18px] font-semibold sm:text-[26px]
-    lg:rotate-[-90deg] lg:origin-[0,0] ">
-      {title}
-    </h2>
-  ):
-  <div className="absolute bottom-0  p-8 justify-center items-center w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] ">
-  <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-3xl glassmorphism mb-4 `}>
-      <img  src="./headset.svg"
-        alt="head set"
-        className="w-1/2 h-1/2 object-contain"
-      />
-    </div>
-    <p className="font-normal leading-5 text-4 text-white uppercase ">
-        Enter the MetaVerse
-    </p>
-    <h2 className="mt-6 font-semibold  sm:text-8 text-6 text-white">
- {title}
-    </h2>
-  </div>}
+    <img
+      src={imgUrl}
+      alt="world"
+      className="absolute w-full h-full object-cover rounded-[24px]"
+    />
+    {active !== id ? (
+      <h2 className="text-white absolute z-0 text-[18px] font-semibold sm:text-[26px]
+    lg:rotate-[-90deg] lg:origin-[0,0] "
+      >
+        {title}
+      </h2>
+    )
+      : (
+        <div className="absolute bottom-0  p-8 justify-center items-center w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] ">
+          <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-3xl glassmorphism mb-4 `}>
+            <img
+              src="./headset.svg"
+              alt="head set"
+              className="w-1/2 h-1/2 object-contain"
+            />
+          </div>
+          <p className="font-normal leading-5 text-4 text-white uppercase ">
+            Enter the MetaVerse
+          </p>
+          <h2 className="mt-6 font-semibold  sm:text-8 text-6 text-white">
+            {title}
+          </h2>
+        </div>
+      )}
 
   </motion.div>
 );
--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,28 +1,35 @@
 'use client';
-import { motion } from "framer-motion";
-import { fadeIn } from "../utils/motion";
-const InsightCard = ({imgUrl,title,subtitle,index}) => (
-  <motion.div 
-  variants={fadeIn("up","spring",index*0.5,1)}
 
-  className='flex felx-col md:flex-row gap-3'>
-      <img src={imgUrl} 
-        alt='card'
-        className="md:w-[230px] w-full h-[200px] rounded-[32px] object-cover"
-      />
-      <div className="flex justify-center items-center w-full">
-          <div className="flex-1 md:ml-[63px] flex-col max-w-[650px]">
-              <h4 className="text-white font-normal lg:text-8 text-6 ">{title}</h4>
-              <p className="mt-4 font-normal text[14px] lg:text-[20px] text-secondary-white">{subtitle}</p>
-          </div>
-          <div className="lg:flex hidden justify-center items-center w-[100px] h-[100px] rounded-full border-white border-[1px] bg-transparent">
-              <img src="arrow.svg"
-              alt="arrow"
-              className="h-[40%] w-[40%] object-contain" />
+import { motion } from 'framer-motion';
+import { fadeIn } from '../utils/motion';
 
-          </div>
+const InsightCard = ({
+  imgUrl, title, subtitle, index,
+}) => (
+  <motion.div
+    variants={fadeIn('up', 'spring', index * 0.5, 1)}
+    className="flex felx-col md:flex-row gap-3"
+  >
+    <img
+      src={imgUrl}
+      alt="card"
+      className="md:w-[230px] w-full h-[200px] rounded-[32px] object-cover"
+    />
+    <div className="flex justify-center items-center w-full">
+      <div className="flex-1 md:ml-[63px] flex-col max-w-[650px]">
+        <h4 className="text-white font-normal lg:text-8 text-6 ">{title}</h4>
+        <p className="mt-4 font-normal text[14px] lg:text-[20px] text-secondary-white">{subtitle}</p>
       </div>
-    
+      <div className="lg:flex hidden justify-center items-center w-[100px] h-[100px] rounded-full border-white border-[1px] bg-transparent">
+        <img
+          src="arrow.svg"
+          alt="arrow"
+          className="h-[40%] w-[40%] object-contain"
+        />
+
+      </div>
+    </div>
+
   </motion.div>
 );
 
