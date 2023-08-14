0a6d584fb4e710cfbbe8d97227764d1b9158c9a3
yassin <y.attuo@esi-sba.dz>
2023-08-14 13:30:53 +0200

    test

--- components/WorldCard.jsx
+++ components/WorldCard.jsx
@@ -1,46 +1,38 @@
 import React from 'react';
 
 const WorldCard = ({ cardName, styleCard, num }) => (
-  <div className={`absolute ${styleCard} w-[196px] rounded-lg h-[150px] shadow-lg shadow-fuchsia-700  p-[6px] bg-[#5d6680] `}>
-
-    <img
-      src={cardName}
-      alt="people 02"
-      className="w-full h-full object-contain"
-    />
-    <div className="absolute top-[50%] left-[10%] z-20 w-[30px] h-[30px] p-[2px] rounded-full bg-[#5d6680]">
-
-      <img
-        src="people-03.png"
-        alt="people 02"
-        className="w-full h-full object-contain"
-      />
-
-    </div>
-    <div className="absolute top-[50%] left-[18%] z-20 w-[30px] h-[30px]  p-[2px] rounded-full bg-[#5d6680]">
-
-      <img
-        src="people-03.png"
-        alt="people 02"
-        className="w-full h-full object-contain"
-      />
-
-    </div>
-    <div className="absolute top-[50%] left-[26%] z-20 w-[30px] h-[30px] p-[2px] rounded-full bg-[#5d6680]">
-
-      <img
-        src="people-03.png"
-        alt="people 02"
-        className="w-full h-full object-contain"
-      />
-
-    </div>
-    <div className="text-white pt-2 py-auto font-normal text-[14px] w-[60%] absolute top-[50%] left-[40%]">
-      <p> + {num}  has joined</p>
-    </div>
-    
-
-  </div>
+     <div
+          className={`absolute ${styleCard} w-[196px] rounded-lg h-[150px] shadow-lg shadow-fuchsia-700  p-[6px] bg-[#5d6680] `}>
+          <img
+               src={cardName}
+               alt='people 02'
+               className='w-full h-full object-contain'
+          />
+          <div className='absolute top-[50%] left-[10%] z-20 w-[30px] h-[30px] p-[2px] rounded-full bg-[#5d6680]'>
+               <img
+                    src='people-03.png'
+                    alt='people 02'
+                    className='w-full h-full object-contain'
+               />
+          </div>
+          <div className='absolute top-[50%] left-[18%] z-20 w-[30px] h-[30px]  p-[2px] rounded-full bg-[#5d6680]'>
+               <img
+                    src='people-03.png'
+                    alt='people 02'
+                    className='w-full h-full object-contain'
+               />
+          </div>
+          <div className='absolute top-[50%] left-[26%] z-20 w-[30px] h-[30px] p-[2px] rounded-full bg-[#5d6680]'>
+               <img
+                    src='people-03.png'
+                    alt='people 02'
+                    className='w-full h-full object-contain'
+               />
+          </div>
+          <div className='text-white pt-2 py-auto font-normal text-[14px] w-[60%] absolute top-[50%] left-[40%]'>
+               <p> + {num} has joined</p>
+          </div>
+     </div>
 );
 
 export default WorldCard;
---
+++ pages/test.js
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    // Replace these hardcoded values with your actual credentials
+    const phoneNumber = '+213663838507';
+    const password = 'pass';
+
+    // Simulate a login by sending a POST request to your login endpoint
+    fetch('http://localhost:8080/api/auth/login/admin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        phoneNumber: phoneNumber,
+        password: password,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Login failed');
+        }
+      })
+      .then((data) => {
+        // Assume the response contains the tokens
+        const tokens = data;
+        console.log(tokens);
+        // Store refresh token in a cookie
+
+        // Now you can use the access token for authenticated requests
+      })
+      .catch((error) => {
+        console.error('Login failed:', error);
+      });
+
+  }, []);
+
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+      <div className='w-full max-w-md p-6 bg-white rounded-md shadow-md'>
+        {/* No UI elements needed in this test */}
+         hi al
+      </div>
+    </div>
+  );
+}
